1864c3cc77eba422d10ac41f09d2c3dc0622cbee
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2025-07-14 05:57:29 +0700

    fisrt commit

---
+++ server/server-81/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Đây là server 81
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
