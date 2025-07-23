c17f869fb3c8b73896fbddf321c2256b5969a39c
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2025-07-23 13:59:46 +0700

    add router for server

--- server/server-81/src/App.js
+++ server/server-81/src/App.js
@@ -1,16 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+function Server81() {
+  return <h2>Đây là server-81</h2>;
+}
+
+function Home() {
+  return <h2>Đây là trang chủ</h2>;
+}
+
+function Account() {
+  return <h2>Đây là trang quản lý tài khoản</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Đây là server 81
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<Server81 />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
